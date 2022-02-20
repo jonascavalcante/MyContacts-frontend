0f1dd3feff35c6a5f087b1db525ef7fd7b40697a
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-02-20 20:29:06 -0300

    Pegando os dados e lidando com o submit do form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import { ButtonContainer, Form } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -9,23 +10,52 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
 
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-
-      <FormGroup error="O formato do e-mail é inválido.">
-        <Input placeholder="E-mail" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
           <option value="linkedin">LinkedIn</option>
         </Select>
