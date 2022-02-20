494bb595f871dd8d9dda92cb4a278a3e5e45a021
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-02-20 10:12:39 -0300

    Criando os componentes Input e Select

---
+++ src/components/Select.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+  padding: 0 16px;
+
+  font-size: 16px;
+  border: 2px solid #FFF;
+  outline: none;
+  border-radius: 4px;
+
+  background-color: #FFF;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  transition: 0.2 ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,17 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="instagram">Instagram</option>
+        <option value="linkedin">LinkedIn</option>
+      </Select>
+    </>
   );
 }
