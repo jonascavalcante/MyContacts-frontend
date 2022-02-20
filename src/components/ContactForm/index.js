87c448ae1b501169d016ea0538539824405034b9
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-02-20 20:52:35 -0300

    Validando os dados do input de nome

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -14,14 +14,32 @@ export default function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const handleSubmit = (event) => {
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => ([
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatório.' },
+      ]));
+    } else {
+      setErrors((prevState) => (
+        prevState.filter((error) => (error.field !== 'name'))
+      ));
+    }
+  }
+
+  console.log(errors);
+
+  function handleSubmit(event) {
     event.preventDefault();
 
     console.log({
       name, email, phone, category,
     });
-  };
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -30,7 +48,7 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           placeholder="Nome"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
         />
       </FormGroup>
 
