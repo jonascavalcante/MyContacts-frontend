ebce692ac16e29eec20977d21e48976b20b38989
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-10-07 22:25:30 -0300

    Melhorando o uso dos React Portals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import { Container, Footer, Overlay } from './styles';
 
+import ReactPortal from '../ReactPortal';
 import Button from '../Button';
 
 export default function Modal({
@@ -12,36 +12,37 @@ export default function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
 
-        <div className="modal-body">{children}</div>
+          <div className="modal-body">{children}</div>
 
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
 
-          <Button
-            type="button"
-            danger={danger}
-            isLoading={isLoading}
-            onClick={onConfirm}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
+            <Button
+              type="button"
+              danger={danger}
+              isLoading={isLoading}
+              onClick={onConfirm}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
 
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
