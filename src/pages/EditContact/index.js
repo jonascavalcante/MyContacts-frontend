5ab4950f046447db428905438ba02bae9c97da80
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-10-07 23:23:11 -0300

    Resolvendo os problemas da página de edição com custom hooks

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -6,11 +6,16 @@ import ContactForm from '../../components/ContactForm';
 import Loader from '../../components/Loader';
 
 import ContactsService from '../../services/ContactsService';
+
 import toast from '../../utils/toast';
+
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
 
 export default function EditContact() {
   const { id } = useParams();
   const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
   const contactFormRef = useRef(null);
 
   const [isLoading, setIsLoading] = useState(true);
@@ -21,21 +26,25 @@ export default function EditContact() {
       try {
         const contact = await ContactsService.getContactById(id);
 
-        contactFormRef.current.setFieldsValues(contact);
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
 
-        setIsLoading(false);
-        setContactName(contact.name);
+          setIsLoading(false);
+          setContactName(contact.name);
+        });
       } catch {
-        history.push('/');
-        toast({
-          type: 'danger',
-          text: 'Contato não encontrado!',
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
         });
       }
     }
 
     loadContact();
-  }, [id, history]);
+  }, [id, history, safeAsyncAction]);
 
   async function handleSubmit(formData) {
     try {
