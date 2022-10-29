f3b16bf0046cdefd575c62a344006d4b643600bc
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-10-29 20:13:37 -0300

    Iniciando a criação do ContactMapper

---
+++ src/services/mappers/ContactMapper.js
@@ -0,0 +1,16 @@
+class ContactMapper {
+  toPersistence(domainContact) {
+    return {
+      name: domainContact.name,
+      email: domainContact.email,
+      phone: domainContact.phone,
+      category_id: domainContact.categoryId,
+    };
+  }
+
+  // toDomain(persistence) {
+  //   return {};
+  // }
+}
+
+export default new ContactMapper();
