92f302d44f53bf684497084132b23075472043eb
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-10-31 08:08:24 -0300

    Animando a entrada do Modal com keyframes

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,4 +1,22 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from{
+    opacity: 0;
+  }
+  to {
+    opacity: 100%;
+  }
+`;
+
+const scaleIn = keyframes`
+  from{
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   width: 100%;
@@ -14,6 +32,8 @@ export const Overlay = styled.div`
 
   background: rgba(0,0,0,0.6);
   backdrop-filter: blur(5px);
+
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -24,6 +44,8 @@ export const Container = styled.div`
   border-radius: 4px;
   background: #FFF;
   box-shadow: 0px 4px 10px rgba(0,0,0,0.04);
+
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
